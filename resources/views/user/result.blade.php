19291c27ae86df2cb751ca594521bd1670153520
Milad <yousefimiladdd@gmail.com>
2024-06-21 23:50:14 +0330

    first commit

--- resources/views/user/result.blade.php
+++ resources/views/user/result.blade.php
@@ -9,13 +9,21 @@
         با توجه به داده ها و اطلاعات وارد شده احتمال ابتلا بیمار به بیماری سیروز کبدی وجود دارد
     </div>
 </div>
-@else
+@elseif ($prediction==0)
 <div class="card border border-success " style="background-color: rgb(179, 255, 160)">
     <div class="card-header">
         سیستم پیش بینی بیماری سیروز کبدی
     </div>
     <div class="card-body text-center">
         با توجه به داده ها و اطلاعات وارد شده احتمال ابتلا بیمار به بیماری سیروز کبدی وجود ندارد
+    </div>
+</div>
+@else
+<div class="card border border-danger " style="background-color: rgb(255, 160, 160)">
+   
+    <div class="card-body text-center">
+        خطای در سیستم رخ داده است 
+        {{$prediction}}
     </div>
 </div>
 @endif
